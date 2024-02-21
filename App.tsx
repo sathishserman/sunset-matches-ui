4d78dca5ad415e715e440a7ce165e2703be12869
joshuasunset <joshua@sunsetmatches.com>
2024-02-21 16:05:39 +0100

    Remove unnecessary imports from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./navigation/Navigation";
 import { Provider } from "react-redux";
 import store from "./redux/store"; // Make sure the path is correct
